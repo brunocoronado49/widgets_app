6b8018b2c334787e056c306b714ba4231dd3299d
brunocoronado49 <francorangelcoronado@gmail.com>
2024-08-20 23:17:37 -0600

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
@@ -18,3 +19,4 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
