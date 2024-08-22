93e8a726646f3c9efe1f480167c5181c764d65d2
brunocoronado49 <francorangelcoronado@gmail.com>
2024-08-22 16:47:15 -0600

    go router configurado

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -45,9 +46,7 @@ class _CustomListTile extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
-      onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
-      },
+      onTap: () => context.push(menuItem.link),
     );
   }
 }
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:widgets_app/presentation/screens/button_screen.dart';
+export 'package:widgets_app/presentation/screens/card_screen.dart';
+export 'package:widgets_app/presentation/screens/home_screen.dart';
+
