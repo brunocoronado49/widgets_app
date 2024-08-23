a18ec535ef99b4e8b6a472f44f68c243e4a6a7a2
brunocoronado49 <francorangelcoronado@gmail.com>
2024-08-23 12:41:20 -0600

    crenado nuevas pantallas y exportandolas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/presentation/screens/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls_screen.dart';
 export 'package:widgets_app/presentation/screens/button_screen.dart';
 export 'package:widgets_app/presentation/screens/card_screen.dart';
 export 'package:widgets_app/presentation/screens/home_screen.dart';
